ac7b7bb27b6f0f4868ae87b5c25d41a2b2d29df8
max <max>
2024-12-02 12:37:12 +0100

    Add MongoDB support to the project
    Updated `Store.Repository.csproj` to include `MongoDB.Driver` version `3.0.0` for MongoDB interaction. Added using directives for `Microsoft.Extensions.Options` and `MongoDB.Driver` in `Program.cs`. Modified `Program` class to configure and register MongoDB services, including reading configuration into `MongoDbConfig` and registering `IMongoClient` as a singleton. Introduced `MongoDbConfig` class in `Store.Repository.DbConfig` namespace for MongoDB connection settings.

---
+++ Store.Repository/DbConfig/MongoDbConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Store.Repository.DbConfig;
+
+public class MongoDbConfig
+{
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+}
--- Store/Program.cs
+++ Store/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 using Store.Repository.DbConfig;
 using Store.Repository.Repositories;
 using Store.Repository.Repositories.Impl;
@@ -16,12 +18,24 @@ public class Program
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        // MySql
         builder.Services.AddDbContext<AppDbContext>(options =>
         {
             options.UseMySql(
                 builder.Configuration.GetConnectionString("DefaultConnection"),
                 new MySqlServerVersion(new Version(9, 1, 0)));
         });
+
+        // Mongo Db
+        builder.Services.Configure<MongoDbConfig>(
+        builder.Configuration.GetSection("MongoDB"));
+        builder.Services.AddSingleton<IMongoClient>(sp =>
+        {
+            var settings = sp.GetRequiredService<IOptions<MongoDbConfig>>().Value;
+            return new MongoClient(settings.ConnectionString);
+        });
+
         builder.Services.AddScoped<IClientRepository, ClientRepository>();
         builder.Services.AddScoped<IClientService, ClientService>();
         builder.Services.AddMemoryCache();
