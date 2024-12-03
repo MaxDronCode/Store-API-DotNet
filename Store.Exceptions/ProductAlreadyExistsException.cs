14f9cdf2b4d6f5aa076cfb9f5e045f0e3f83844f
max <max>
2024-12-03 12:52:15 +0100

    Add ProductAlreadyExistsException to Store.Exceptions
    Introduced a new custom exception class `ProductAlreadyExistsException` in the `Store.Exceptions` namespace. This class handles cases where a product with a duplicate name is added. It includes three constructors: a parameterless one with a default message, one that accepts a custom message, and one that accepts a custom message and an inner exception.

---
+++ Store.Exceptions/ProductAlreadyExistsException.cs
@@ -0,0 +1,16 @@
+﻿namespace Store.Exceptions;
+
+public class ProductAlreadyExistsException : Exception
+{
+    public ProductAlreadyExistsException() : base("Product with this name already exists")
+    {
+    }
+
+    public ProductAlreadyExistsException(string? message) : base(message)
+    {
+    }
+
+    public ProductAlreadyExistsException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
