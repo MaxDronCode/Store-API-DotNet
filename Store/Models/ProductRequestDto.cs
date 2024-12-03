609a0c6d7709dce5b1cbf3e94925d74287e9517d
max <max>
2024-12-03 11:24:11 +0100

    Add validation to ProductRequestDto; add ProductoResponseDto
    - Added [Required] and [MaxLength(20)] attributes to the Name property in ProductRequestDto to enforce validation.
    - Introduced ProductoResponseDto class with Name and Code properties for response handling.

--- Store/Models/ProductRequestDto.cs
+++ Store/Models/ProductRequestDto.cs
@@ -1,7 +1,12 @@
-﻿namespace Store.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.Api.Models;
 
 public class ProductRequestDto
 {
+    [Required]
+    [MaxLength(20)]
     public string Name { get; set; }
+
     public int Code { get; set; }
 }
---
+++ Store/Models/ProductoResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Store.Api.Models;
+
+public class ProductoResponseDto
+{
+    public string Name { get; set; }
+    public int Code { get; set; }
+}
