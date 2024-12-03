a4bad6eefa537fd02de0b22fc2cb52a155c881df
max <max>
2024-12-03 13:04:28 +0100

    Add ProductNotFoundException to Store.Exceptions namespace
    Introduced a new custom exception class `ProductNotFoundException`
    within the `Store.Exceptions` namespace. This class includes three
    constructors: a parameterless constructor, a constructor that
    accepts a nullable string `message`, and a constructor that accepts
    a nullable string `message` and a nullable `Exception` `innerException`.

---
+++ Store.Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace Store.Exceptions;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException() : base()
+    {
+    }
+
+    public ProductNotFoundException(string? message) : base(message)
+    {
+    }
+
+    public ProductNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
