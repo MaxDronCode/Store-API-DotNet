052461a2d693fff61c01b5026962a99a9887a53d
max <max>
2024-12-03 12:27:37 +0100

    Add Store.Shared project and ProductsController class
    - Added Store.Shared project to Store.sln with build configurations.
    - Introduced ProductsController in Store.Api.Controllers.
    - ProductsController includes CreateProduct method for POST requests.

---
+++ Store/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Api.Models;
+using Store.Service.Services;
+
+namespace Store.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct(ProductRequestDto productDto)
+    {
+        var createdProduct = await _productService.AddProduct(productDto);
+    }
+}
