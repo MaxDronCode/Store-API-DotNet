fd7ba53f8d9765ed491eb688954f4fe808e9fadf
max <max>
2024-12-03 12:38:14 +0100

    Refactor ProductService for logging and async support
    - Add ILogger<ProductService> for logging
    - Correct IProductRespository to IProductRepository
    - Update constructor to include ILogger and use ProductRepository
    - Move CodeSize constant within the class
    - Make AddProduct method asynchronous with try-catch for exceptions
    - Use await for adding product and map using ProductMappings
    - Log exceptions in AddProduct method

--- Store.Service/Services/Impl/ProductService.cs
+++ Store.Service/Services/Impl/ProductService.cs
@@ -1,21 +1,26 @@
-﻿using NanoidDotNet;
+﻿using Microsoft.Extensions.Logging;
+using NanoidDotNet;
 using Store.Api.Models;
+using Store.Repository;
+using Store.Repository.Repositories.Impl;
+using Store.Service.Mappings;
 using Store.Service.Models;
 
 namespace Store.Service.Services.Impl;
 
 public class ProductService : IProductService
 {
-    private readonly IProductRespository _productRespository;
-
-    public ProductService(IProductRespository productRespository)
-    {
-        _productRespository = productRespository;
-    }
-
+    private readonly IProductRepository _productRepository;
+    private readonly ILogger<ProductService> _logger;
     private const int CodeSize = 10;
 
-    public Task<Product> AddProduct(ProductRequestDto productDto)
+    public ProductService(ProductRepository productRepository, ILogger<ProductService> logger)
+    {
+        _productRepository = productRepository;
+        _logger = logger;
+    }
+
+    public async Task<Product> AddProduct(ProductRequestDto productDto)
     {
         var generatedCode = Nanoid.Generate(size: CodeSize);
         var newProduct = new Product
@@ -23,5 +28,15 @@ public class ProductService : IProductService
             Code = generatedCode,
             Name = productDto.Name
         };
+        try
+        {
+            var savedEntity = await _productRepository.AddProduct(ProductMappings.ToEntity(newProduct));
+            return ProductMappings.ToModel(savedEntity);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error while trying to add product with name {Name}", productDto.Name);
+            throw;
+        }
     }
 }
