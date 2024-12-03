ba0b8a665e9fbbe897f57bdd6bfdae44eb8799dd
max <max>
2024-12-03 12:56:47 +0100

    Add logging and product retrieval to ProductsController
    Updated using directives to include System.Text.RegularExpressions and re-ordered them. Added a private _logger field and updated the constructor to accept an ILogger<ProductsController> parameter. Introduced a new GetProduct method to log requests, validate product codes, and handle product retrieval with appropriate responses. Added a private isValidCode method for product code validation using regex.

--- Store/Controllers/ProductsController.cs
+++ Store/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
 using Store.Api.Mappings;
 using Store.Api.Models;
 using Store.Exceptions;
@@ -12,10 +13,12 @@ namespace Store.Api.Controllers;
 public class ProductsController : ControllerBase
 {
     private readonly IProductService _productService;
+    private readonly ILogger<ProductsController> _logger;
 
-    public ProductsController(IProductService productService)
+    public ProductsController(IProductService productService, ILogger<ProductsController> logger)
     {
         _productService = productService;
+        _logger = logger;
     }
 
     [HttpPost]
@@ -32,5 +35,34 @@ public class ProductsController : ControllerBase
         }
         var responseDto = ProductMappings.ToResponseDto(createdProduct);
         return CreatedAtAction(nameof(GetProduct), new { code = responseDto.Code }, responseDto);
+    }
+
+    [HttpGet("{code}")]
+    public async Task<IActionResult> GetProduct(string code)
+    {
+        _logger.LogInformation("Request for obtaining product with code {Code}", code);
+
+        if (!isValidCode(code))
+        {
+            _logger.LogWarning("Invalid code recieved: {Code}.", code);
+            return BadRequest("Invalid code format.");
+        }
+
+        Product product;
+        try
+        {
+            product = await _productService.GetProductByCode(code);
+        }
+        catch (ProductNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        var responseDto = ProductMappings.ToResponseDto(product);
+        return Ok(responseDto);
+    }
+
+    private bool isValidCode(string code)
+    {
+        return Regex.IsMatch(code, @"^[a-zA-Z0-9]{10}$");
     }
 }
