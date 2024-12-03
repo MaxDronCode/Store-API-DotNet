4120e60d321bc7ed3842ea148dc4179dd7dec88b
max <max>
2024-12-03 12:32:43 +0100

    Add ProductMappings.cs for Product and ProductEntity mapping
    Introduce ProductMappings.cs to map between Product and ProductEntity
    objects. This file includes methods ToEntity and ToModel to facilitate
    conversion between service and repository layers, enhancing modularity
    and maintainability.

---
+++ Store.Service/Mappings/ProductMappings.cs
@@ -0,0 +1,25 @@
+﻿using Store.Repository.Models;
+using Store.Service.Models;
+
+namespace Store.Service.Mappings;
+
+public static class ProductMappings
+{
+    public static ProductEntity ToEntity(Product product)
+    {
+        return new ProductEntity
+        {
+            Code = product.Code,
+            Name = product.Name
+        };
+    }
+
+    public static Product ToModel(ProductEntity entity)
+    {
+        return new Product
+        {
+            Code = entity.Code,
+            Name = entity.Name
+        };
+    }
+}
