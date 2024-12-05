d13ace3461afdbcbd7e9ddefe1a4c08e140513fd
max <max>
2024-12-05 11:11:27 +0100

    Add Sales and SaleDetails DbSets to AppDbContext
    Updated AppDbContext.cs to include two new DbSet properties: Sales and SaleDetails. These properties are of types SaleEntity and SaleDetailEntity respectively, enabling the AppDbContext to manage and query these entities in the database.

--- Store.Repository/DbConfig/AppDbContext.cs
+++ Store.Repository/DbConfig/AppDbContext.cs
@@ -12,4 +12,8 @@ public class AppDbContext : DbContext
     public DbSet<ClientEntity> Clients { get; set; }
 
     public DbSet<ProductEntity> Products { get; set; }
+
+    public DbSet<SaleEntity> Sales { get; set; }
+
+    public DbSet<SaleDetailEntity> SaleDetails { get; set; }
 }
