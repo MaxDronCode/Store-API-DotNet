1b28dd0c4382acfb5597b938945583d544f63c1d
max <max>
2024-12-05 09:31:00 +0100

    Add SaleDetailEntity class with EF mappings
    Introduce SaleDetailEntity class in Store.Repository.Models.
    - Decorate with data annotations for EF mapping.
    - Define SaleId and ProductCode as primary keys.
    - Map SaleId to sale_id column, referencing SaleEntity.Id.
    - Map ProductCode to product_code column, referencing ProductEntity.Code.
    - Add Quantity property mapped to quantity column.

---
+++ Store.Repository/Models/SaleDetailEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.Repository.Models;
+
+public class SaleDetailEntity
+{
+    [Key]
+    [Column("sale_id")]
+    [ForeignKey(nameof(SaleEntity.Id))]
+    public int SaleId { get; set; }
+
+    [Key]
+    [Column("product_code")]
+    [ForeignKey(nameof(ProductEntity.Code))]
+    public string ProductCode { get; set; }
+
+    [Column("quantity")]
+    public int Quantity { get; set; }
+}
