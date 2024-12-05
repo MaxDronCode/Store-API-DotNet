8a274ee91274a27b795034d88879a18f572d5b8c
max <max>
2024-12-05 09:27:13 +0100

    Add SaleEntity class mapped to sales_cab table
    Introduced a new class `SaleEntity` in the `Store.Repository.Models` namespace.
    The `SaleEntity` class is mapped to the `sales_cab` database table using the `[Table("sales_cab")]` attribute.
    The class includes the following properties:
    - `Id`: Primary key with `[Key]` and `[DatabaseGenerated(DatabaseGeneratedOption.Identity)]` attributes.
    - `ClientNif`: String property mapped to `client_nif` column with a foreign key relationship to `ClientEntity.Nif`.
    - `SellDate`: `DateTime` property mapped to `sell_date` column.
    Added necessary using directives for `System.ComponentModel.DataAnnotations` and `System.ComponentModel.DataAnnotations.Schema`.

---
+++ Store.Repository/Models/SaleEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.Repository.Models;
+
+[Table("sales_cab")]
+public class SaleEntity
+{
+    [Key]
+    [Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Column("client_nif")]
+    [ForeignKey(nameof(ClientEntity.Nif))]
+    public string ClientNif { get; set; }
+
+    [Column("sell_date")]
+    public DateTime SellDate { get; set; }
+}
