32bae9ff67332c1be007e6f2d17230defddbc48b
max <max>
2024-12-09 13:24:28 +0100

    Add IncomeReportDto class to Store.Api.Models namespace
    A new data transfer object (DTO) class named `IncomeReportDto` has been
    added to the `Store.Api.Models` namespace. This class is designed to
    hold information about income reports, including the product code,
    product name, and total sales.

---
+++ Store/Models/IncomeReportDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.Api.Models;
+
+public class IncomeReportDto
+{
+    public string ProductCode { get; set; }
+    public string ProductName { get; set; }
+    public int TotalSales { get; set; }
+}
