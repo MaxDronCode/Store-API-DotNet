b6eeccb518eaf811b2f4169acd777c27b63321bc
max <max>
2024-12-09 13:25:09 +0100

    Add ProductSalesInfo class to Store.Service.Models namespace
    Introduced a new class `ProductSalesInfo` within the `Store.Service.Models` namespace. This class includes properties for `ProductCode`, `ProductName`, and `TotalSales` to store information about product sales.

---
+++ Store.Service/Models/ProductSalesInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.Service.Models;
+
+public class ProductSalesInfo
+{
+    public string ProductCode { get; set; }
+    public string ProductName { get; set; }
+    public int TotalSales { get; set; }
+}
