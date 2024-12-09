78385141ce4c363823d79b97c242383c252a6b2d
max <max>
2024-12-09 08:20:34 +0100

    Add SalesController to handle sales creation requests
    Introduced SalesController in Store.Api.Controllers namespace.
    - Added necessary using directives for various namespaces.
    - Decorated SalesController with [ApiController] and [Route].
    - Implemented constructor to initialize _saleService and _logger.
    - Added CreateSale action method to handle POST requests.
      - Mapped SaleRequestDto to Sale model.
      - Handled ClientNotFoundException and ProductNotFoundException.
      - Logged other exceptions and returned 500 status code.
      - Mapped created sale to response DTO and returned CreatedAtAction.

---
+++ Store/Controllers/SalesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Api.Exceptions;
+using Store.Api.Mappings;
+using Store.Api.Models;
+using Store.Exceptions;
+using Store.Service.Models;
+using Store.Service.Services;
+
+namespace Store.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SalesController : ControllerBase
+{
+    private readonly ISaleService _saleService;
+    private readonly ILogger<SalesController> _logger;
+
+    public SalesController(ISaleService saleService, ILogger<SalesController> logger)
+    {
+        _saleService = saleService;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateSale(SaleRequestDto saleDto)
+    {
+        var sale = SaleMappings.ToModel(saleDto);
+        Sale createdSale;
+        try
+        {
+            createdSale = await _saleService.CreateSale(sale);
+        }
+        catch (ClientNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (ProductNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error creating sale");
+            return StatusCode(500, e.Message);
+        }
+
+        var responseDto = SaleMappings.ToDto(createdSale);
+
+        return CreatedAtAction(nameof(CreateSale), responseDto);
+    }
+}
