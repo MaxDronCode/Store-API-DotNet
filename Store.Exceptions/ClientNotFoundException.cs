08b78351902f66323448aeed17c97a6ac7b48308
max <max>
2024-12-09 08:22:09 +0100

    Add new constructors to ClientNotFoundException class
    The ClientNotFoundException class has been updated to include two new constructors:
    - A parameterless constructor `ClientNotFoundException()` that calls the base class constructor with no arguments.
    - A constructor `ClientNotFoundException(string? message, Exception? innerException)` that allows for an optional message and an optional inner exception to be passed, calling the base class constructor with these parameters.
    
    These changes provide more flexibility in how the exception can be instantiated, accommodating different scenarios where an exception might need to be thrown with varying levels of detail.

--- Store.Exceptions/ClientNotFoundException.cs
+++ Store.Exceptions/ClientNotFoundException.cs
@@ -5,4 +5,12 @@ public class ClientNotFoundException : Exception
     public ClientNotFoundException(string message) : base(message)
     {
     }
+
+    public ClientNotFoundException() : base()
+    {
+    }
+
+    public ClientNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
 }
