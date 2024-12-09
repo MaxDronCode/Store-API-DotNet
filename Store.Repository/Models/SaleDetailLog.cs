26db23cd783ae5c683feba385720327c328f1890
max <max>
2024-12-09 12:53:24 +0100

    Add SaleDetailLog and SaleLogEntity classes
    Two new classes have been added to the project:
    
    1. SaleDetailLog.cs:
       - Introduced namespace Store.Repository.Models.
       - Added SaleDetailLog class with properties: ProductName (string) and Quantity (int).
    
    2. SaleLogEntity.cs:
       - Imported MongoDB.Bson and MongoDB.Bson.Serialization.Attributes namespaces.
       - Introduced namespace Store.Repository.Models.
       - Added SaleLogEntity class with properties: Id (string) with BsonId and BsonRepresentation(BsonType.ObjectId) attributes, SaleId (int), ClientNif (string), SellDate (DateTime), and Items (List<SaleDetailLog>).

---
+++ Store.Repository/Models/SaleDetailLog.cs
@@ -0,0 +1,7 @@
+﻿namespace Store.Repository.Models;
+
+public class SaleDetailLog
+{
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ Store.Repository/Models/SaleLogEntity.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Store.Repository.Models;
+
+public class SaleLogEntity
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public int SaleId { get; set; }
+    public string ClientNif { get; set; }
+    public DateTime SellDate { get; set; }
+    public List<SaleDetailLog> Items { get; set; }
+}
