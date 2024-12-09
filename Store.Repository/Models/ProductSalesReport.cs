e3be0152f430effff206bc619c5aa8cc2b4ca46f
max <max>
2024-12-09 13:25:42 +0100

    Add ProductSalesReport class to Store.Repository.Models
    Introduced a new namespace `Store.Repository.Models` and added a
    `ProductSalesReport` class within it. This class includes three
    properties: `ProductCode` (string), `ProductName` (string), and
    `TotalSold` (int), to represent a report of product sales.

---
+++ Store.Repository/Models/ProductSalesReport.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.Repository.Models;
+
+public class ProductSalesReport
+{
+    public string ProductCode { get; set; }
+    public string ProductName { get; set; }
+    public int TotalSold { get; set; }
+}
