a878aaacdbbae79cd80322cf78389ae9cb74f7c6
max <max>
2024-12-09 12:53:41 +0100

    Add MongoDB repository for handling sale logs
    Introduced a new repository interface and its implementation for
    handling sale logs in a MongoDB database.
    
    - Added `IMongoSaleLogRepository` interface with methods
      `AddSaleLog` and `GetSaleLogsByClientNif`.
    - Implemented `MongoSaleLogRepository` class that uses MongoDB
      driver to interact with the database, including methods for
      adding sale logs and retrieving them by client's NIF.

---
+++ Store.Repository/Repositories/IMongoSaleLogRepository.cs
@@ -0,0 +1,10 @@
+﻿using Store.Repository.Models;
+
+namespace Store.Repository.Repositories;
+
+public interface IMongoSaleLogRepository
+{
+    Task AddSaleLog(SaleLogEntity entity);
+
+    Task<IEnumerable<SaleLogEntity>> GetSaleLogsByClientNif(string clientNif);
+}
---
+++ Store.Repository/Repositories/Impl/MongoSaleLogRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Store.Repository.DbConfig;
+using Store.Repository.Models;
+
+namespace Store.Repository.Repositories.Impl;
+
+public class MongoSaleLogRepository : IMongoSaleLogRepository
+{
+    private readonly IMongoCollection<SaleLogEntity> _saleLogs;
+
+    public MongoSaleLogRepository(IMongoClient mongoClient, IOptions<MongoDbConfig> config)
+    {
+        var database = mongoClient.GetDatabase(config.Value.DatabaseName);
+        _saleLogs = database.GetCollection<SaleLogEntity>("saleLogs");
+    }
+
+    public async Task AddSaleLog(SaleLogEntity entity)
+    {
+        await _saleLogs.InsertOneAsync(entity);
+    }
+
+    public async Task<IEnumerable<SaleLogEntity>> GetSaleLogsByClientNif(string clientNif)
+    {
+        return await _saleLogs.Find(s => s.ClientNif == clientNif).ToListAsync();
+    }
+}
