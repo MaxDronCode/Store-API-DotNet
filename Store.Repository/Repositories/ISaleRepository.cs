2f4a63348f40b0bc5746363cf8ee81f74aa7757e
max <max>
2024-12-09 08:22:53 +0100

    Add Sale repository interface and implementation
    Introduce ISaleRepository interface with CreateSale method.
    Implement SaleRepository class with dependency injection for
    AppDbContext and ILogger<ProductRepository>. Add CreateSale
    method to handle sale creation, including error handling and
    logging.

---
+++ Store.Repository/Repositories/ISaleRepository.cs
@@ -0,0 +1,8 @@
+﻿using Store.Repository.Models;
+
+namespace Store.Repository.Repositories;
+
+public interface ISaleRepository
+{
+    Task<SaleEntity> CreateSale(SaleEntity sale);
+}
---
+++ Store.Repository/Repositories/Impl/SaleRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using Store.Repository.DbConfig;
+using Store.Repository.Models;
+
+namespace Store.Repository.Repositories.Impl;
+
+public class SaleRepository : ISaleRepository
+{
+    private readonly AppDbContext _context;
+    private readonly ILogger<ProductRepository> _logger;
+
+    public SaleRepository(AppDbContext context, ILogger<ProductRepository> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task<SaleEntity> CreateSale(SaleEntity sale)
+    {
+        try
+        {
+            _context.Sales.Add(sale);
+            await _context.SaveChangesAsync();
+            _logger.LogInformation("Sale created successfully");
+            return sale;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error while trying to create sale");
+            throw;
+        }
+    }
+}
