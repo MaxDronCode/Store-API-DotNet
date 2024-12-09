24fa12bc14bc9a9721fad2487229da81b1612aee
max <max>
2024-12-09 08:23:29 +0100

    Add ISaleService interface and SaleService implementation
    Introduce ISaleService interface with CreateSale method.
    Add SaleService class implementing ISaleService.
    Inject ISaleRepository, IClientRepository, IProductRepository, and ILogger<SaleService> into SaleService.
    Implement CreateSale method to handle client and product retrieval, error logging, and sale creation.

---
+++ Store.Service/Services/ISaleService.cs
@@ -0,0 +1,8 @@
+﻿using Store.Service.Models;
+
+namespace Store.Service.Services;
+
+public interface ISaleService
+{
+    public Task<Sale> CreateSale(Sale sale);
+}
---
+++ Store.Service/Services/Impl/SaleService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using Store.Api.Exceptions;
+using Store.Exceptions;
+using Store.Repository;
+using Store.Repository.Repositories;
+using Store.Service.Mappings;
+using Store.Service.Models;
+
+namespace Store.Service.Services.Impl;
+
+public class SaleService : ISaleService
+{
+    private readonly ISaleRepository _saleRepository;
+    private readonly IClientRepository clientRepository;
+    private readonly IProductRepository productRepository;
+    private readonly ILogger<SaleService> _logger;
+
+    public SaleService(ISaleRepository saleRepository, IClientRepository clientRepository, IProductRepository productRepository, ILogger<SaleService> logger)
+    {
+        _saleRepository = saleRepository;
+        this.clientRepository = clientRepository;
+        this.productRepository = productRepository;
+        _logger = logger;
+    }
+
+    public async Task<Sale> CreateSale(Sale sale)
+    {
+        var client = await clientRepository.GetClientByNif(sale.ClientNif);
+
+        if (client == null)
+        {
+            _logger.LogError($"Client with NIF {sale.ClientNif} not found");
+            throw new ClientNotFoundException($"Client with nif {sale.ClientNif} not found.");
+        }
+
+        foreach (var item in sale.Items)
+        {
+            var product = await productRepository.GetProductByName(item.ProductName);
+
+            if (product == null)
+            {
+                _logger.LogError($"Product with name {item.ProductName} not found");
+                throw new ProductNotFoundException($"Product with name {item.ProductName} not found.");
+            }
+
+            item.ProductCode = product.Code;
+        }
+
+        var saleEntity = SaleMappings.ToEntity(sale);
+
+        var createdSaleEntity = await _saleRepository.CreateSale(saleEntity);
+
+        var createdSale = SaleMappings.ToModel(createdSaleEntity);
+
+        return createdSale;
+    }
+}
