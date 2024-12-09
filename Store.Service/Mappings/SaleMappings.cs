3331c6ead0e46bac02dc9758c44f4ba4b498d83a
max <max>
2024-12-09 08:23:51 +0100

    Add SaleMappings.cs for Sale and SaleEntity conversions
    Introduce SaleMappings.cs to map between Sale and SaleEntity objects.
    Define ToEntity and ToModel methods for conversion.
    ToEntity maps ClientNif and Items properties.
    ToModel maps Id, ClientNif, and SaleDetails properties.

---
+++ Store.Service/Mappings/SaleMappings.cs
@@ -0,0 +1,34 @@
+﻿using Store.Repository.Models;
+using Store.Service.Models;
+
+namespace Store.Service.Mappings;
+
+public static class SaleMappings
+{
+    public static SaleEntity ToEntity(Sale sale)
+    {
+        return new SaleEntity
+        {
+            ClientNif = sale.ClientNif,
+            SaleDetails = sale.Items.Select(i => new SaleDetailEntity
+            {
+                Quantity = i.Quantity,
+                ProductCode = i.ProductCode
+            }).ToList()
+        };
+    }
+
+    public static Sale ToModel(SaleEntity saleEntity)
+    {
+        return new Sale
+        {
+            Id = saleEntity.Id,
+            ClientNif = saleEntity.ClientNif,
+            Items = saleEntity.SaleDetails.Select(i => new SaleDetail
+            {
+                ProductCode = i.ProductCode,
+                Quantity = i.Quantity
+            }).ToList()
+        };
+    }
+}
